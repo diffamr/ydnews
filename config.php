72ce4f6d27ca81e4bc98ad610c08162cf9765651
diffamr <44928835+diffamr@users.noreply.github.com>
2018-12-01 01:30:00 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $config = [
-"imei" => "xxxxxxxx",
-"sign" => "xxxxxxxx",
-"token" => "xxxxxxxx",
-"uuid" => "xxxxxxxx"
+"imei" => "868229033624939&isemu=2&lang=in-ID&market=googleplay&max_id=0&page=1&phoneBrand=Xiaomi&pushType=xiaomi&refresh=1",
+"sign" => "8fe2ffef7afba9ac6361ff7661c8d61cf0ff94f4",
+"token" => "c6133ae6e9cdf6fddebf0476cdca67f9",
+"uuid" => "29fd0acc-3b30-402d-99f9-421886fdd7ac"
 ];
