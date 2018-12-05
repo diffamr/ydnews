3d21d0d3670059312769c6466b069f873a3a3ddf
diffamr <44928835+diffamr@users.noreply.github.com>
2018-12-05 21:45:07 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $config = [
 "imei" => "868229033624939",
-"sign" => "8dc4f16d48a0a7abf43a39831f3adc1e6dc538e6",
+"sign" => "45807eecfd678b1541f9087954f3df653a231b44",
 "token" => "fa9ddfe8f27f948737fb925c5608bc50",
 "uuid" => "32a07cd6-c0bf-40cd-9c53-f060a140c60e"
 ];
